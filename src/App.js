b04b23e90660d410a31a249f31129e87da2b8001
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2024-04-23 14:01:23 -0400

    Create About page.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SilentHill from "./Pages/SilentHill";
 import ALF from "./Pages/ALF";
 import Cthulu from "./Pages/Cthulu";
 import Resthaven from "./Pages/Resthaven";
+import About from "./Pages/About";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/animalliberationfront" element={<ALF />} />
       <Route path="/callofcthulu" element={<Cthulu />} />
       <Route path="resthaven" element={<Resthaven />}/> 
+      <Route path="about" element={<About />}></Route>
     </Routes>
     </>
   );
---
+++ src/Pages/About.js
@@ -0,0 +1,11 @@
+import NavBar from "../Components/NavBar";
+
+const About = () => {
+    return (
+        <>
+        <NavBar />
+        </>
+    )
+}
+
+export default About;

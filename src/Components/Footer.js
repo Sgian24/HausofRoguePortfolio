6372033678ac8151b46b5cc7841669dddb141870
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2024-06-10 22:33:33 -0400

    Make footer links open in new window.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
           <footer className="px-0">
             <Row className="mb-3">
              <div className="d-flex gap-5 px-2"> 
-              <a className="footer-links" href="https://ca.linkedin.com/in/billieroa">LinkedIn</a>
-              <a className="footer-links" href="https://www.behance.net/billieroa">Behance</a>
-              <a className="footer-links" href="https://dribbble.com/hausofrogue/">Dribble</a>
+              <a className="footer-links" href="https://ca.linkedin.com/in/billieroa" target="_blank">LinkedIn</a>
+              <a className="footer-links" href="https://www.behance.net/billieroa" target="_blank">Behance</a>
+              <a className="footer-links" href="https://dribbble.com/hausofrogue/" target="_blank">Dribble</a>
              </div>
             </Row>
             <Row className="px-0">

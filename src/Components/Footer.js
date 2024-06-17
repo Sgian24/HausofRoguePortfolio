5888d3c2d1c1abd99932075c46cd7fadfe48d65e
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2024-06-17 12:22:57 -0400

    Fix small screen resolution responsive issues.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <Container className="footer-container bg-black d-flex justify-content-center align-items-center" fluid>  
           <footer className="px-0">
             <Row className="mb-3">
-             <div className="d-flex gap-5 px-2"> 
+             <div className="footer-link-container d-flex gap-5 px-2"> 
               <a className="footer-links" href="https://ca.linkedin.com/in/billieroa" target="_blank">LinkedIn</a>
               <a className="footer-links" href="https://www.behance.net/billieroa" target="_blank">Behance</a>
               <a className="footer-links" href="https://dribbble.com/hausofrogue/" target="_blank">Dribble</a>
